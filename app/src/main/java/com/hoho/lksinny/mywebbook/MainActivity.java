76c33f785d24879effd539bde70deada60bdf5d6
lksinny <lksinny@gmail.com>
2015-07-25 19:26:43 +0900

    WebBook First Commit_1507251925

--- app/src/main/java/com/hoho/lksinny/mywebbook/MainActivity.java
+++ app/src/main/java/com/hoho/lksinny/mywebbook/MainActivity.java
@@ -4,14 +4,40 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
+
+  TextView textViewTest;
+  Button buttonTest;
+  View.OnClickListener cListener;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    textViewTest = (TextView)findViewById(R.id.textViewTest);
+    buttonTest = (Button)findViewById(R.id.buttonTest);
+
+    cListener = new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        //TODO Auto-generated method stub
+        switch (v.getId()) {
+          case R.id.buttonTest:
+            textViewTest.setText(getString(R.string.clickbutton));
+            break;
+          default:
+            break;
+        }
+      }
+    };
+
+    buttonTest.setOnClickListener(cListener);
   }
 
   @Override
